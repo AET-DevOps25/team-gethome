cb53fe992e0d8b449b6fed7659b75aa40895abcf
Julio Emmanuel Meza Rangel <juliomeza2510@outlook.com>
2025-05-20 21:48:51 +0200

    Add initial Docker configuration

--- service/emergency-service/src/main/java/com/example/gethome/emergency/controller/HomeController.java
+++ service/emergency-service/src/main/java/com/example/gethome/emergency/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.gethome.gethome.controller;
+package com.example.gethome.emergency.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
