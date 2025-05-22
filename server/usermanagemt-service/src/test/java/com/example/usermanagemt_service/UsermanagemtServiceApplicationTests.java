bee312490d34d08c3f75ad5ab3521432611690b9
Parth <parth.karkar@tum.de>
2025-05-22 13:51:09 +0200

    user management service intialised

---
+++ server/usermanagemt-service/src/test/java/com/example/usermanagemt_service/UsermanagemtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usermanagemt_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagemtServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
